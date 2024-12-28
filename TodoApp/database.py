6a445b8b3495b0dd187c1ee7c40a59cb5576189b
Amir Mahdi Jamali <mahdijamali101@gmail.com>
2024-12-28 14:58:56 +0330

    Switch to SQLite and prepare for Railway deployment

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,13 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+
+if os.environ.get("RAILWAY_ENVIRONMENT"):
+    SQLALCHEMY_DATABASE_URL = "sqlite:////data/todosapp.db"
+else:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
 
 
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql+pg8000://postgres:mahdi1380@localhost:5432/TodoApplicationDatabase"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
